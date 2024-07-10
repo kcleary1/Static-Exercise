d91701806dd698de915b1e8209035daa572efc0c
Kyle Cleary <kscleary11@gmail.com>
2024-07-10 11:31:50 -0600

    Exercise Complete

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace StaticExercise
+﻿using System;
+
+namespace StaticExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Would you like to convert to Fahrenheit or Celsius?");
+           var unitChosen = Console.ReadLine();
+
+            if (unitChosen == "Fahrenheit")
+
+            {
+                Console.WriteLine("Which temperature in Celsius to you need to convert?");
+                string celsius = Console.ReadLine();
+                double celsiusD = double.Parse(celsius);
+                TempConverter.CelsiusToFahrenheit(celsiusD);
+            }
+            else
+            {
+                Console.WriteLine("Which temperature in Fahrenheit to you need to convert?");
+                string fahrenheit = Console.ReadLine();
+                double fahrenheitD = double.Parse(fahrenheit);
+                TempConverter.FahrenheitToCelsius(fahrenheitD);
+            }
+
+                
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            double celsius = (fahrenheit - 32) * (5 / 9);
+            Console.WriteLine($"Here is the equivalant in Celsius: {celsius}");
+            return celsius;
+        }
+
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            double fahrenheit = (celsius * (9 / 5)) + 32;
+            Console.WriteLine($"Here is the equivalant in Fahrenheit: {fahrenheit}");
+            return fahrenheit;
+
+        }
+
+
+    }
+}
